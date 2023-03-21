b4c28c31d2ec5dabab5e3dfb188b0b243aa61fe0
Deannah Burke <deannah@bierstadtlager.com>
2023-03-21 10:54:24 -0600

    Add first spec for take_turn method

--- lib/round.rb
+++ lib/round.rb
@@ -10,4 +10,10 @@ class Round
     def current_card
         @deck.cards.first
     end
+
+    def take_turn(guess)
+        turn = Turn.new(guess, current_card)
+        @turns.push(turn)
+        @turns.first
+    end
 end
